170bfe18f1b2550a3ba159dec219f97c29059642
DazzzleKK <krivinsony@gmail.com>
2022-12-01 14:39:42 +0700

    Form widgets

--- placesremember/app/forms.py
+++ placesremember/app/forms.py
@@ -1,10 +1,22 @@
 from django import forms
 from .models import Places
-from mapwidgets.widgets import GooglePointFieldWidget  # GoogleStaticOverlayMapWidget
+from mapwidgets.widgets import GooglePointFieldWidget
 
 
 class NewPlaceForm(forms.ModelForm):
     class Meta:
         model = Places
-        fields = ["name", "latitude", "longitude", "location"]
-        widgets = {"location": GooglePointFieldWidget}
+        fields = ["name", "desctiption", "location"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Новое место"}
+            ),
+            "desctiption": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 3,
+                    "placeholder": "(необязательно)",
+                }
+            ),
+            "location": GooglePointFieldWidget,
+        }
--- placesremember/app/models.py
+++ placesremember/app/models.py
@@ -5,16 +5,17 @@ from django.contrib.gis.db.models import PointField
 
 
 class Places(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(verbose_name="Название", max_length=150)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
     )
+    desctiption = models.CharField(verbose_name="Описание", max_length=300, null=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    location = PointField(null=True)
+    location = PointField(verbose_name="Место", null=True)
 
     def __str__(self):
-        return f"Место :{self.name} Автор: {self.user} Локация: {self.latitude} {self.longitude}"
+        return f"Место :{self.name} Автор: {self.user}"
 
 
 class Profile(models.Model):
@@ -22,6 +23,3 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, null=True)
     avatar = models.CharField(max_length=300, null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.user.username} Profile"
