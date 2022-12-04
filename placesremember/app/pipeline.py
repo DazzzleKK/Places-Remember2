65f42cf4dec0ea1cef5adce1db9ee56988b945f4
DazzzleKK <krivinsony@gmail.com>
2022-12-04 16:33:05 +0700

    Google Auth done

--- placesremember/app/pipeline.py
+++ placesremember/app/pipeline.py
@@ -9,6 +9,9 @@ def get_info(backend, response, user=None, *args, **kwargs):
     if backend.name == "vk-oauth2":
         url = response.get("photo", "")
         name = response.get("first_name")
+    elif backend.name == "google-oauth2":
+        url = response.get("picture")
+        name = response.get("given_name")
 
     if url and name:
         Profile.objects.update_or_create(user=user, name=name, avatar=url)
